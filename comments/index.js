8f6c51fe70cdc8eb64a4a967b700d974b3f4cb9b
Artem <arstrel@gmail.com>
2021-06-14 09:07:21 -0400

    First commit

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const { randomBytes } = require('crypto');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const comments = commentsByPostId[id] || [];
+
+  res.send(comments);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { id } = req.params;
+  const { content } = req.body;
+
+  const comments = commentsByPostId[id] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001');
+});
