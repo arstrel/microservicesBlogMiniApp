e7ab30a619c05b68d1844ef6f806a7e4f295722f
Artem <arstrel@gmail.com>
2021-07-11 17:23:49 -0700

    add eventBus storage and query service event syncing

--- eventBus/index.js
+++ eventBus/index.js
@@ -4,8 +4,13 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
+
   let errorMessage = '';
 
   // post service
@@ -57,6 +62,10 @@ app.post('/events', (req, res) => {
   } else {
     res.send({ status: 'OK' });
   }
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,15 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  console.log('Received event', req.body.type);
-
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -48,6 +41,18 @@ app.post('/events', (req, res) => {
       status,
     };
   }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
+
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   console.log(posts);
 
@@ -56,6 +61,19 @@ app.post('/events', (req, res) => {
   res.end();
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Query service: Listening on port 4002');
+
+  try {
+    // eventBus service
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('Processing event:', event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
