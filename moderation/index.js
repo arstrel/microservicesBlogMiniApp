ebfbd0eb7ad9d3f6afb086aeb2bcf521b0a14082
Artem <arstrel@gmail.com>
2021-08-02 07:36:15 -0700

    updates to moderation

--- moderation/index.js
+++ moderation/index.js
@@ -4,11 +4,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post('/events', async (req, res) => {
-  console.log('Received event', req.body.type);
-
-  const { type, data } = req.body;
-
+const handleEvent = async (type, data) => {
   if (type === 'CommentCreated') {
     const isApproved = !data.content.includes('orange');
     const newComment = {
@@ -31,12 +27,33 @@ app.post('/events', async (req, res) => {
         });
       });
   }
+};
+
+app.post('/events', async (req, res) => {
+  console.log('Received event', req.body.type);
+
+  const { type, data } = req.body;
+
+  await handleEvent(type, data);
 
   res.send({});
   // .end prevents 'socket hang up' axios error on eventBus side
   res.end();
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
   console.log('Moderation service: Listening on port 4003');
+
+  try {
+    // eventBus service
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('Processing event:', event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
